efb9843ccb28bd815b778d65f9ab49b2b8b62aa7
Konstantin Vlasenko <konstantin.vlasenko@quest.com>
2011-02-16 15:46:06 +0300

    handling remoting exceptions

--- client.ps1
+++ client.ps1
@@ -1,5 +1,4 @@
 function script:process_table_remotely($table, $fitnesse){
-	$table | Out-Default
 	$computer = $table[0][4]
 	try {
 		$c = New-Object System.Net.Sockets.TcpClient($computer, 35)
@@ -11,11 +10,8 @@ function script:process_table_remotely($table, $fitnesse){
 		$c.Close() 
 	}
 	catch [System.Exception] {
-		$_.Exception.Message | Out-Default
-		$send = '[000002:' + (slimlen $table[0][0]) + ':' + $table[0][0] + ':' + (slimlen $_.Exception.Message) + ':' + $_.Exception.Message + ':]'
+		$send = '[000002:' + (slimlen $table[0][0]) + ':' + $table[0][0] + ':' + (slimlen "$slimexception$($_.Exception.Message)") + ':' + "$slimexception$($_.Exception.Message)" + ':]'
 		$send = (slimlen $send) + ":" + $send + ":"
-		
-				
 		$send = [text.encoding]::utf8.getbytes((pack_results $send))
 		$fitnesse.Write($send, 0, $send.Length)
 	}
