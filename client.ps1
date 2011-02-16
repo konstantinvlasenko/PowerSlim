2333194a825297cdcbf27e2f41b20950e049799a
Konstantin Vlasenko <konstantin.vlasenko@quest.com>
2011-02-16 17:28:05 +0300

    prepare for multycomputers (not ready yet)

--- client.ps1
+++ client.ps1
@@ -1,13 +1,21 @@
 function script:process_table_remotely($table, $fitnesse){
-	$computer = $table[0][4]
+	$computers = $table[0][4].Split(',')
 	try {
-		$c = New-Object System.Net.Sockets.TcpClient($computer, 35)
-		$remoteserver = $c.GetStream()
-		$remoteserver.Write($slimbuffer, 0, $slimbuffersize)
-		read_message($remoteserver)
+		$result = new-Object 'system.collections.generic.dictionary[string,object]'
+		foreach($computer in $computers){
+			$c = New-Object System.Net.Sockets.TcpClient($computer, 35)
+			$remoteserver = $c.GetStream()
+			$remoteserver.Write($slimbuffer, 0, $slimbuffersize)
+			
+			$t= get_message($remoteserver)
+			$t | Out-Default
+			$result[$computer] = Get-SlimTable($t)
+			$remoteserver.Close()         
+			$c.Close() 
+		}
+		#if($result.Count -eq 1){
 		$fitnesse.Write($slimbuffer, 0, $slimbuffersize)
-		$remoteserver.Close()         
-		$c.Close() 
+		#}
 	}
 	catch [System.Exception] {
 		$send = '[000002:' + (slimlen $table[0][0]) + ':' + $table[0][0] + ':' + (slimlen "$slimexception$($_.Exception.Message)") + ':' + "$slimexception$($_.Exception.Message)" + ':]'
