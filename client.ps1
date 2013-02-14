284e58efbc4f0e9949b378431954d5a47424c6a8
Mike Plavsky <Mike.Plavsky@quest.com>
2013-02-14 21:20:39 +0400

    added One Symbol Test and points where to catch variables in local PowerSlim

--- client.ps1
+++ client.ps1
@@ -5,61 +5,81 @@
 # think this stuff is worth it, you can buy me a beer in return.
 #
 function script:process_table_remotely($table, $fitnesse){
+
 	#$targets = $table[0][4].Trim(',').Split(',')
 	try {
+
 		$originalslimbuffer = $slimbuffer.Clone()
 		$originalslimbuffersize = $slimbuffersize
+
 		$result = new-Object 'system.collections.generic.dictionary[string,object]'
+
 		foreach($t in $targets){ 
+
 			$computer, $port = $t.split(':')
+
 			if($computer.StartsWith('$')){
 				$computer = $slimsymbols[$computer.Substring(1)]
 			}
+
 			if($port -eq $null){$port = 35};
+
 			$computer, $port | Out-Default
 			
 			if($slimsymbols.Count -ne 0){
+
 				$c = New-Object System.Net.Sockets.TcpClient($computer, $port)
 				$remoteserver = $c.GetStream()
 						
 				$list = @($slimsymbols.GetEnumerator() | % {$_})
 				$tr = "[" + (slimlen $list) + ":"
+
 				foreach ($obj in $list){
 									
 					$itemstr = "[" +  (6).ToString("d6") + ":"
+
 					$itemstr += (slimlen 'scriptTable_0_0') + ":scriptTable_0_0:" + (slimlen 'callAndAssign') + ":callAndAssign:"
 					$itemstr += (slimlen $obj.Key) + ":$($obj.Key):" + (slimlen 'scriptTableActor') + ":scriptTableActor:"
 					$itemstr += (slimlen 'eval') + ":eval:"
-					#if($obj -is [string]){
-						$itemstr +=  (($obj.Value.Length + 2).ToString("d6")) + ":'$($obj.Value)':"
-					#}
-					#else{
-					#	$itemstr +=  (slimlen $obj.Value) + ":$($obj.Value):"
-					#}
+
+    				$itemstr +=  (($obj.Value.Length + 2).ToString("d6")) + ":'$($obj.Value)':"
+	
 					$itemstr += "]"
 			
 					$tr += (slimlen $itemstr) + ":" + $itemstr + ":"
 				} 
+
 				$tr += "]"
 				
-				$s2 = [text.encoding]::utf8.getbytes($tr).Length.ToString("d6") + ":" + $tr
-						
+				$s2 = [text.encoding]::utf8.getbytes($tr).Length.ToString("d6") + ":" + $tr						
 				$s2 = [text.encoding]::utf8.getbytes($s2)
+
+                $tr | Out-Default 
+
 				$remoteserver.Write($s2, 0, $s2.Length)
 				get_message($remoteserver)
+
 			}
 						
 			$c = New-Object System.Net.Sockets.TcpClient($computer, $port)
 			$remoteserver = $c.GetStream()
+
+            [text.encoding]::utf8.getstring($originalslimbuffer, 0, $originalslimbuffersize) | Out-Default # here you have to catch ...scriptTable_2_1:000013:callAndAssign:000007:symbolA:
 						
 			$remoteserver.Write($originalslimbuffer, 0, $originalslimbuffersize)
 			$result[$computer] = get_message($remoteserver)
+
+            $result[$computer] | Out-Default # here add to symbols scriptTable_2_1:000015:Returned Symbol:  sctiptTable_2_1 -> ID http://fitnesse.org/FitNesse.UserGuide.SliM.SlimProtocol    
+
 			$remoteserver.Close()         
 			$c.Close() 
+
 		}
+
 		#if($result.Count -eq 1){
 		$fitnesse.Write($slimbuffer, 0, $slimbuffersize)
 		#}
+
 	}
 	catch [System.Exception] {
 		$send = '[000002:' + (slimlen $table[0][0]) + ':' + $table[0][0] + ':' + (slimlen "$slimexception$($_.Exception.Message)") + ':' + "$slimexception$($_.Exception.Message)" + ':]'
