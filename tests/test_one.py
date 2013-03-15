14021609c0573950db70230cc11a7b1df5e5b325
Mike Plavsky <Mike.Plavsky@quest.com>
2013-03-15 20:49:00 +0400

    added streams

--- tests/test_one.py
+++ tests/test_one.py
@@ -19,7 +19,38 @@ def i_(s):
   script(s)
   global res
   res = (p.Invoke())
+
+  p.Commands.Clear()
+
+def print_errors():
  
+  print len(p.Streams.Error)
+  for i in p.Streams.Error:
+    print i
+
+def make_stream(s):
+  
+  i_( "$stream = New-Object System.IO.MemoryStream" )
+  i_( "$writer = New-Object System.IO.StreamWriter $stream" )
+
+  i_( "$writer.WriteLine('%s')" % s )
+  i_( "$writer.Flush()" )
+  i_( "$stream.Seek(0, 'Begin')" )  
+  i_( "Write-Error $stream.Position" )
+
+def test_stream():
+
+  p.Streams.Error.Clear()
+  make_stream( "Wow" )
+
+  i_( "$buff = new-object byte[] 3" )
+  i_( "$stream.Read($buff,0,3)" )
+  i_( "[text.encoding]::utf8.getstring($buff);$buff" )
+
+  print_errors()
+
+  eq_(res[0], "Wow" )
+
 def test_powershell():
   
   i_( "$a=12;$a" )
