665646ec654480fa65e6eebe1e646726b7b44aec
Sergey Kalitin <sergey.kalitin@gmail.com>
2015-04-18 23:15:09 +0300

    "THE BEER-WARE LICENSE

--- slim.ps1
+++ slim.ps1
@@ -2,6 +2,11 @@
 # PowerSlim (Revision 47)#
 ##########################
 
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <konstantin.vlasenko@gmail.com> wrote this file. As long as you retain this notice
+# you can do whatever you want with this stuff. If we meet some day, and you
+# think this stuff is worth it, you can buy me a beer in return.
+
 $slimver = "Slim -- V0.3`n"
 $slimvoid = ''
 $slimsymbols = New-Object -TypeName 'system.collections.generic.dictionary[string,object]'
